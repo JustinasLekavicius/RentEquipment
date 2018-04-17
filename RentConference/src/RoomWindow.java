e6a6f6ae2bb89806e82c4f57e77678eaa0b4e517
Vytenis <alavytenis@gmail.com>
2018-04-17 10:47:20 +0300

    Updated scroller in RoomWindow

--- RentConference/src/RoomWindow.java
+++ RentConference/src/RoomWindow.java
@@ -20,10 +20,10 @@ public class RoomWindow extends Main{
 	public RoomWindow() {
 		frame.setResizable(false);
 		frame.setTitle("Rooms");
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 466, 351);
 		frame.getContentPane().setLayout(null);
 		JButton HelpMeButton = new JButton("Help me choose!");
-		HelpMeButton.setBounds(106, 230, 200, 44);
+		HelpMeButton.setBounds(182, 208, 200, 44);
 		frame.getContentPane().add(HelpMeButton);
 		
 		JButton AuditoriumButton = new JButton("Auditorium");
@@ -39,18 +39,12 @@ public class RoomWindow extends Main{
 		frame.getContentPane().add(RoomButton);
 		
 		JButton BackButton = new JButton("Back");
-		BackButton.setBounds(12, 249, 82, 25);
+		BackButton.setBounds(6, 285, 82, 25);
 		frame.getContentPane().add(BackButton);
 		
 		JButton NextButton = new JButton("Next");
-		NextButton.setBounds(321, 240, 117, 25);
+		NextButton.setBounds(372, 285, 82, 25);
 		frame.getContentPane().add(NextButton);
-		
-		TextArea textArea = new TextArea();
-		textArea.setText("Please select a room");
-		textArea.setEditable(false);
-		textArea.setBounds(134, 38, 304, 186);
-		frame.getContentPane().add(textArea);
 		
 		JButton ClassroomButton = new JButton("Classroom");
 		ClassroomButton.setBounds(12, 174, 117, 25);
@@ -59,6 +53,14 @@ public class RoomWindow extends Main{
 		JButton ChamberButton = new JButton("Chamber");
 		ChamberButton.setBounds(12, 137, 117, 25);
 		frame.getContentPane().add(ChamberButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(142, 26, 278, 170);
+		frame.getContentPane().add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		AuditoriumButton.addActionListener(new ActionListener()
